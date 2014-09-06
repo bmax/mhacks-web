aa7c65a07e4f791f1e1a9211ab5ee63b961a6a64
bmax <bmax1337@gmail.com>
2014-09-06 09:41:48 -0400

    Added some nice UI, added driver(s)

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('login');
+  this.route('contact');
+  this.route('drivers');
+  this.route('driver', { path: 'driver/:driver_id'});
 });
 
 export default Router;
